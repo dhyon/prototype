7db7668157314ec339cf74658cdcaf2d05845974
muhdmud <mudmud.muhd@gmail.com>
2021-09-23 00:17:03 +0800

    initial logic

--- pages/api/data/staratlas/markets.ts
+++ pages/api/data/staratlas/markets.ts
@@ -1,17 +1,54 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { Account, Connection, PublicKey } from '@solana/web3.js';
+import { Market } from '@project-serum/serum';
 
 type Data = {
     name: string
 }
+let connection = new Connection('https://solana-api.projectserum.com');
 
-export default function handler(
+let marketAddress = new PublicKey('EJ8MX3M4xsgAn8uZkLNyp76zAqn9uY18NtitASoYoRyS');
+// mainnet program id
+let programAddress = new PublicKey("9xQeWvG816bUx9EPjHmaT23yvVM2ZWbrrpZb9PusVFin");
+
+export default async (
     req: NextApiRequest,
     res: NextApiResponse<Data>
-) {
+) => {
     console.log(req.query)
+    //let market = await Market.load(connection, marketAddress, {}, programAddress);
+
     res.status(200).json({ name: 'lotsa markets' })
     // TODO: this should return data on all existing markets
     // Note: data source needs to be cached
     // query params:
     // category = structure | cosmetic | access | ship | crew | equipment
+
+    // // Fetching orderbooks
+    // let bids = await market.loadBids(connection);
+    // let asks = await market.loadAsks(connection);
+    // // L2 orderbook data
+    // for (let [price, size] of bids.getL2(20)) {
+    // console.log(price, size);
+    // }
+
+    // // Retrieving fills
+    // for (let fill of await market.loadFills(connection)) {
+    //     console.log(fill.orderId, fill.price, fill.size, fill.side);
+    // }
+
+    let url = 'https://galaxy.staratlas.com/nfts';
+
+    fetch(url)
+    .then(res => res.json())
+    .then((out) => {
+    console.log(out);
+    })
+    .catch(err => { throw err });
+
+    // fetch url data
+    // make data struct with name, marketid, total supply and attributes
+
+
+  
 }
--- pages/api/data/staratlas/markets/[marketId].ts
+++ pages/api/data/staratlas/markets/[marketId].ts
@@ -1,14 +1,29 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { Account, Connection, PublicKey } from '@solana/web3.js';
+import { Market } from '@project-serum/serum';
 
 type Data = {
     name: string
 }
 
-export default function handler(
+let connection = new Connection('https://solana-api.projectserum.com');
+
+// mainnet program id
+let programAddress = new PublicKey("9xQeWvG816bUx9EPjHmaT23yvVM2ZWbrrpZb9PusVFin");
+
+export default async (
     req: NextApiRequest,
     res: NextApiResponse<Data>
-) {
+) => {
     const marketId = req.query.marketId as string
+    let marketAddress = new PublicKey(marketId);
+    let market = await Market.load(connection, marketAddress, {}, programAddress);
+
     res.status(200).json({ name: marketId })
     // TODO: this should return data on the specific marketId
+
+    // fetch all fills for item with this id
+    // determine total volume and attach timestamps to fills
+    // fetch more data from galaxy url?
+
 }
