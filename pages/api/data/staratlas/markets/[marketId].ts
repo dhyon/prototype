de047a1d05f55606e12ad7df172923baf2866cbb
muhdmud <mudmud.muhd@gmail.com>
2021-09-25 00:55:07 +0800

    update

--- pages/api/data/staratlas/markets/[marketId].ts
+++ pages/api/data/staratlas/markets/[marketId].ts
@@ -8,6 +8,11 @@ const CONNECTION = new Connection('https://solana-api.projectserum.com');
 // mainnet program id
 const SERUM_V3_PROGRAM_ADDRESS = new PublicKey("9xQeWvG816bUx9EPjHmaT23yvVM2ZWbrrpZb9PusVFin");
 
+function randomIntFromInterval(min, max) { 
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+  
+  
 export default async (
     req: NextApiRequest,
     res: NextApiResponse
@@ -15,36 +20,39 @@ export default async (
     const marketId = req.query.marketId as string
 
     let marketAddress = new PublicKey(marketId);
-    // check if marketid doesnt exist on mainnet
-    // const ifMarketExists = await CONNECTION.getAccountInfo(marketAddress);
-    // if (!ifMarketExists) {
-    //     res.status(400).json({ error: 'Could not find market' });
-    // }
-    
     let market = await Market.load(CONNECTION, marketAddress, {}, SERUM_V3_PROGRAM_ADDRESS);
     const fills = await market.loadFills(CONNECTION);
 
     let lowest = Number.MAX_SAFE_INTEGER;
     let highest = 0;
     let totalCalculatedVolume = 0;
-    let totalCalculatedFillSize = 0;
+    let totalSize = 0;
     var sortedPrices:string[] = [] 
+    let fakeDate = new Date();
+    let currUniqueHolders = randomIntFromInterval(500, 10000);
 
     const marketData = fills
     .filter(f => f.side === 'buy')
-    .map(f => { 
+    .map(f => {
+        fakeDate.setDate(fakeDate.getDate()-1);
         if(lowest > f.price)
             lowest = f.price;
         if(highest < f.price)
             highest = f.price;
-        totalCalculatedFillSize++;
+        totalSize+= f.size;
         totalCalculatedVolume+= f.price;
-        sortedPrices.push(f.price);
-        return { id : f.orderId, price : f.price, size : f.size}
+        sortedPrices.push(f.price)
+        let myData = {
+            'orderId': f.orderId,
+            'price': f.price,
+            'size': f.size,
+            'timestamp': fakeDate.valueOf()
+        }
+        return myData
     });
-
+    totalCalculatedVolume = totalSize / marketData.length
     sortedPrices.sort();
-    res.status(200).json(  { marketid: marketId, totalFillSize : totalCalculatedFillSize,
+    res.status(200).json(  { marketid: marketId, totalFillSize : marketData.length,
     totalVolume : totalCalculatedVolume, aggregatedData : marketData, sortedPriceFills : sortedPrices,
-    allTimeHigh : highest, allTimeLow : lowest } );
+    allTimeHigh : highest, allTimeLow : lowest, uniqueHolders : currUniqueHolders } );
 }
