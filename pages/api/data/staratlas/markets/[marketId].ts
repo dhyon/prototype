1ea0655f7881d30d734d168fd67e44644363a49d
muhdmud <mudmud.muhd@gmail.com>
2021-09-25 01:01:48 +0800

    fix typings

--- pages/api/data/staratlas/markets/[marketId].ts
+++ pages/api/data/staratlas/markets/[marketId].ts
@@ -1,14 +1,14 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { Connection, PublicKey } from '@solana/web3.js';
-import { Market } from '@project-serum/serum';
-import { getSortedRoutes } from 'next/dist/shared/lib/router/utils';
+import { Connection, PublicKey } from '@solana/web3.js'
+import { Market } from '@project-serum/serum'
+import { getSortedRoutes } from 'next/dist/shared/lib/router/utils'
 
 
-const CONNECTION = new Connection('https://solana-api.projectserum.com');
+const CONNECTION = new Connection('https://solana-api.projectserum.com')
 // mainnet program id
-const SERUM_V3_PROGRAM_ADDRESS = new PublicKey("9xQeWvG816bUx9EPjHmaT23yvVM2ZWbrrpZb9PusVFin");
+const SERUM_V3_PROGRAM_ADDRESS = new PublicKey("9xQeWvG816bUx9EPjHmaT23yvVM2ZWbrrpZb9PusVFin")
 
-function randomIntFromInterval(min, max) { 
+function randomIntFromInterval(min : number, max : number) { 
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
   
@@ -19,28 +19,28 @@ export default async (
 ) => {
     const marketId = req.query.marketId as string
 
-    let marketAddress = new PublicKey(marketId);
-    let market = await Market.load(CONNECTION, marketAddress, {}, SERUM_V3_PROGRAM_ADDRESS);
-    const fills = await market.loadFills(CONNECTION);
+    let marketAddress = new PublicKey(marketId)
+    let market = await Market.load(CONNECTION, marketAddress, {}, SERUM_V3_PROGRAM_ADDRESS)
+    const fills = await market.loadFills(CONNECTION)
 
-    let lowest = Number.MAX_SAFE_INTEGER;
-    let highest = 0;
-    let totalCalculatedVolume = 0;
-    let totalSize = 0;
+    let lowest = Number.MAX_SAFE_INTEGER
+    let highest = 0
+    let totalCalculatedVolume = 0
+    let totalSize = 0
     var sortedPrices:string[] = [] 
-    let fakeDate = new Date();
-    let currUniqueHolders = randomIntFromInterval(500, 10000);
+    let fakeDate = new Date()
+    let currUniqueHolders = randomIntFromInterval(500, 10000)
 
     const marketData = fills
     .filter(f => f.side === 'buy')
     .map(f => {
-        fakeDate.setDate(fakeDate.getDate()-1);
+        fakeDate.setDate(fakeDate.getDate()-1)
         if(lowest > f.price)
-            lowest = f.price;
+            lowest = f.price
         if(highest < f.price)
-            highest = f.price;
-        totalSize+= f.size;
-        totalCalculatedVolume+= f.price;
+            highest = f.price
+        totalSize+= f.size
+        totalCalculatedVolume+= f.price
         sortedPrices.push(f.price)
         let myData = {
             'orderId': f.orderId,
@@ -49,11 +49,11 @@ export default async (
             'timestamp': fakeDate.valueOf()
         }
         return myData
-    });
+    })
     totalCalculatedVolume = totalSize / marketData.length
-    sortedPrices.sort();
+    sortedPrices.sort()
     res.status(200).json(  { marketid: marketId, totalFillSize : marketData.length,
     totalVolume : totalCalculatedVolume, recentFills : marketData, sortedPriceFills : sortedPrices,
-    allTimeHigh : highest, allTimeLow : lowest, uniqueHolders : currUniqueHolders } );
+    allTimeHigh : highest, allTimeLow : lowest, uniqueHolders : currUniqueHolders } )
 
 }
