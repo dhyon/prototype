75a1574d69690687f4428a9ee5b5af4c18864f7d
muhdmud <mudmud.muhd@gmail.com>
2021-09-25 00:03:04 +0800

    added anonymous functions

--- pages/api/data/staratlas/markets/[marketId].ts
+++ pages/api/data/staratlas/markets/[marketId].ts
@@ -3,65 +3,48 @@ import { Connection, PublicKey } from '@solana/web3.js';
 import { Market } from '@project-serum/serum';
 import { getSortedRoutes } from 'next/dist/shared/lib/router/utils';
 
-type Data = {
-    marketId : string;
-    totalFillSize : number;
-    totalVolume: number;
-    // id, timestamp, price, size, side
-    arrayData : Array<[id :string, timestamp :number, price : number, size : number, side : string]>;
-    sortedPriceFills : Array<any>;
-    allTimeHigh : number;
-    allTimeLow : number;
-};
 
 const CONNECTION = new Connection('https://solana-api.projectserum.com');
 // mainnet program id
-const PROGRAM_ADDRESS = new PublicKey("9xQeWvG816bUx9EPjHmaT23yvVM2ZWbrrpZb9PusVFin");
+const SERUM_V3_PROGRAM_ADDRESS = new PublicKey("9xQeWvG816bUx9EPjHmaT23yvVM2ZWbrrpZb9PusVFin");
 
 export default async (
     req: NextApiRequest,
-    res: NextApiResponse<Data>
+    res: NextApiResponse
 ) => {
-    const MARKET_ID = req.query.marketId as string
-    //const MARKET_ID_TEST = "8vycGWEqhvnTJR4aTcN2TvtUUNtLXoCTrpCGtoD8R4bo"; 
-    
-    let marketAddress = new PublicKey(MARKET_ID);
+    const marketId = req.query.marketId as string
+
+    let marketAddress = new PublicKey(marketId);
     // check if marketid doesnt exist on mainnet
-    const ifMarketExists = await CONNECTION.getAccountInfo(marketAddress);
-    if (ifMarketExists === null) {
-      throw new Error('Could not find market');
-    }
+    // const ifMarketExists = await CONNECTION.getAccountInfo(marketAddress);
+    // if (!ifMarketExists) {
+    //     res.status(400).json({ error: 'Could not find market' });
+    // }
     
-    let market = await Market.load(CONNECTION, marketAddress, {}, PROGRAM_ADDRESS);
+    let market = await Market.load(CONNECTION, marketAddress, {}, SERUM_V3_PROGRAM_ADDRESS);
+    const fills = await market.loadFills(CONNECTION);
 
-    // default initialize
-    let marketData : Data = {
-        marketId : MARKET_ID,
-        totalFillSize : 0, 
-        totalVolume : 0,
-        arrayData : [],  
-        sortedPriceFills : [],
-        allTimeHigh : 0,
-        allTimeLow : 0,
-    };
-    
+    let lowest = Number.MAX_SAFE_INTEGER;
+    let highest = 0;
+    let totalCalculatedVolume = 0;
+    let totalCalculatedFillSize = 0;
+    var sortedPrices:string[] = [] 
 
-    // Retrieving fills
-    for (let fill of await market.loadFills(CONNECTION)) {
-        console.log(fill.orderId, fill.price, fill.size, fill.side);
-        marketData.totalFillSize++;
-        marketData.arrayData.push([fill.orderId, Date.now() + 10,fill.price, fill.size, fill.side]);
-        if(fill.side === "buy")
-            marketData.sortedPriceFills.push(fill.price);
-    }
-    // processing data metrics
-    marketData.sortedPriceFills.sort();
-    for (var i in marketData.sortedPriceFills) {
-        marketData.totalVolume += marketData.sortedPriceFills[i];
-    }
-    let fillSize = marketData.sortedPriceFills.length;
-    marketData.allTimeHigh = marketData.sortedPriceFills[fillSize-1];
-    marketData.allTimeLow = marketData.sortedPriceFills[0];
-    
-    res.status(200).json({ marketData })
+    const marketData = fills
+    .filter(f => f.side === 'buy')
+    .map(f => { 
+        if(lowest > f.price)
+            lowest = f.price;
+        if(highest < f.price)
+            highest = f.price;
+        totalCalculatedFillSize++;
+        totalCalculatedVolume+= f.price;
+        sortedPrices.push(f.price);
+        return { id : f.orderId, price : f.price, size : f.size}
+    });
+
+    sortedPrices.sort();
+    res.status(200).json(  { marketid: marketId, totalFillSize : totalCalculatedFillSize,
+    totalVolume : totalCalculatedVolume, aggregatedData : marketData, sortedPriceFills : sortedPrices,
+    allTimeHigh : highest, allTimeLow : lowest } );
 }
