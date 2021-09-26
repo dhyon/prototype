ae3f1e7bbea734bf655c3890099081da9cdd98dd
Sam Ullman <samullman@gmail.com>
2021-09-26 16:16:32 -0700

    page layout

--- components/game-card.js
+++ components/game-card.js
@@ -1,30 +1,26 @@
-import {Heading,  Box, Image, Button, } from '@chakra-ui/react';
-import Link from "next/link";
+import { Heading, Box, Image, Button } from '@chakra-ui/react';
+import Link from 'next/link';
 
 function GameCard({ el }) {
-  debugger
+  debugger;
   return (
-    <Link href={ "/game/" + el.slug }>
-      <Box rounded="lg" overflow="hidden" borderWidth={1}>
-    <Box key={el._id}  height="220px" cursor="pointer"  rounded="md" >
-      <Image src={ el.image } height="100%" width="100%" objectFit="cover"  />
-    </Box>
+    <Link href={'/game/' + el.slug}>
+      <Box cursor="pointer" borderWidth={1} rounded="lg" display="inline-block" position="relative">
+        <Image
+          src="/star-atlas.jpg"
+          height="400px"
+          maxWidth="800px"
+          rounded="lg"
+          objectFit="cover"
+          width="100%"
+        />
 
-    <Box bgGradient="linear(-145deg, #FB933C, #FEC60F)" variant="outline" height="4px" >
-    </Box>
-
-      <Box p={3}>
-
-    <Heading size="md">
-    {el.name}
-    </Heading>
-
-
-    </Box>
-
-   
-    </Box>
-
+        <Box position="absolute" width="100%" p={10} bottom={0}>
+          <Heading color="white" letterSpacing={1}>
+            {el.name}
+          </Heading>
+        </Box>
+      </Box>
     </Link>
   );
 }
--- pages/games.tsx
+++ pages/games.tsx
@@ -12,12 +12,12 @@ const Home: NextPage = () => {
 
   return (
     <Layout title="All games">
-      <Box px={[5, 5, 10]}>
-        <Heading fontWeight="bold" textTransform="uppercase" size="lg" mb={[4, 4, 8]}>
+      <Box p={[5, 5, 8]}>
+        <Heading fontWeight="bold" textTransform="uppercase" mb={[5, 5,]}>
           All games
         </Heading>
 
-        <SimpleGrid columns={[1, 1, 2]} spacing={[4, 4, 8]}>
+        <SimpleGrid columns={[1, 1, 1]} spacing={[4, 4, 8]}>
           {state.games.map((el: any) => {
             return <GameCard key={el._id} el={el} />;
           })}
