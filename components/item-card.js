142d60a1d6e73e4369ef4f59f75777d192827ef0
Danny H <hyperformance@gmail.com>
2021-09-30 11:05:01 -0400

    reduce image quality for ItemCard

--- components/item-card.js
+++ components/item-card.js
@@ -7,8 +7,7 @@ function ItemCard({ el }) {
     <Link href={`/item/` + el._id}>
       <Box rounded="md" overflow="hidden" borderWidth={1}>
         <Box key={el._id} height="125px" cursor="pointer" roundedTop="md" position="relative" overflow="hidden">
-          {/* <Image src={el.image} height="100%" width="100%" objectFit="cover" /> */}
-          <NextImage src={ el.image }  width="400" height="300" objectFit="cover"  />
+          <NextImage src={ el.image } layout="fill" quality={25} objectFit="cover"/>
         </Box>
 
         <Box p={3}>
