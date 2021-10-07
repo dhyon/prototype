8b218079b6e7c92a5094a63961e216f72a34e667
Muhd <32418600+muhdmud@users.noreply.github.com>
2021-10-07 10:12:39 +0800

    update branch (#28)

--- components/footer.js
+++ components/footer.js
@@ -1,16 +1,110 @@
-import { Heading, Divider, Box, Image, Button, useColorModeValue } from '@chakra-ui/react';
-import Link from 'next/link';
+import { ReactNode } from 'react';
+import {
+  Box,
+  Container,
+  Stack,
+  SimpleGrid,
+  Text,
+  Link,
+  VisuallyHidden,
+  chakra,
+  useColorModeValue,
+} from '@chakra-ui/react';
+import { FaTwitter, FaYoutube, FaInstagram } from 'react-icons/fa';
 
-function Footer() {
-  const bg = useColorModeValue('gray.50', 'gray.900');
+const ListHeader = ({ children }) => {
   return (
-    <Box>
-      <Divider />
-      <Box p={[5, 5, 8]}>
-        <Heading>Footer</Heading>
+    <Text fontWeight={'500'} fontSize={'lg'} mb={2}>
+      {children}
+    </Text>
+  );
+};
+
+const SocialButton = ({
+  children,
+  label,
+  href,
+}) => {
+  return (
+    <chakra.button
+      bg={useColorModeValue('blackAlpha.100', 'whiteAlpha.100')}
+      rounded={'full'}
+      w={8}
+      h={8}
+      cursor={'pointer'}
+      as={'a'}
+      href={href}
+      display={'inline-flex'}
+      alignItems={'center'}
+      justifyContent={'center'}
+      transition={'background 0.3s ease'}
+      _hover={{
+        bg: useColorModeValue('blackAlpha.200', 'whiteAlpha.200'),
+      }}>
+      <VisuallyHidden>{label}</VisuallyHidden>
+      {children}
+    </chakra.button>
+  );
+};
+
+export default function Footer() {
+  return (
+    <Box
+      bg={useColorModeValue('gray.50', 'gray.900')}
+      color={useColorModeValue('gray.700', 'gray.200')}>
+      <Container as={Stack} maxW={'4xl'} py={10}>
+        <SimpleGrid columns={{ base: 1, sm: 2, md: 4 }} spacing={150}>
+          <Stack align={'flex-start'}>
+            <ListHeader>Company</ListHeader>
+            <Link href={'#'}>About Us</Link>
+            <Link href={'#'}>Blog</Link>
+            <Link href={'#'}>Careers</Link>
+            <Link href={'#'}>Contact Us</Link>
+          </Stack>
+
+          <Stack align={'flex-start'}>
+            <ListHeader>Support</ListHeader>
+            <Link href={'#'}>Tutorials</Link>
+            <Link href={'#'}>Documentation</Link>
+            <Link href={'#'}>Discord</Link>
+          </Stack>
+
+          <Stack align={'flex-start'}>
+            <ListHeader>Product</ListHeader>
+            <Link href={'#'}>Pricing</Link>
+            <Link href={'#'}>Partners</Link>
+            <Link href={'#'}>Something</Link>
+          </Stack>
+
+        </SimpleGrid>
+      </Container>
+
+      <Box
+        borderTopWidth={1}
+        borderStyle={'solid'}
+        borderColor={useColorModeValue('gray.200', 'gray.700')}>
+        <Container
+          as={Stack}
+          maxW={'4xl'}
+          py={4}
+          direction={{ base: 'column', md: 'row' }}
+          spacing={1}
+          justify={{ md: 'space-between' }}
+          align={{ md: 'center' }}>
+          <Text>© 2021 Project Titan. All rights reserved</Text>
+          <Stack direction={'row'} spacing={5}>
+            <SocialButton label={'Twitter'} href={'https://solana.com/ignition'}>
+              <FaTwitter />
+            </SocialButton>
+            <SocialButton label={'YouTube'} href={'https://solana.com/ignition'}>
+              <FaYoutube />
+            </SocialButton>
+            <SocialButton label={'Instagram'} href={'https://solana.com/ignition'}>
+              <FaInstagram />
+            </SocialButton>
+          </Stack>
+        </Container>
       </Box>
     </Box>
   );
 }
-
-export default Footer;
