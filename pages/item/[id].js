7a0d5c7831ed1b8d923dc24f97b31719544fce9d
Sam Ullman <samullman@gmail.com>
2021-10-10 21:35:46 -0700

    item page

--- pages/item/[id].js
+++ pages/item/[id].js
@@ -1,19 +1,34 @@
 // import type { NextPage } from 'next';
 // import Image from 'next/image'
-import { VictoryChart, VictoryScatter, VictoryAxis } from 'victory';
+import { VictoryChart, VictoryZoomContainer,VictoryLine, VictoryBrushContainer, VictoryAxis } from 'victory';
 import Rarity from '../../components/rarity';
 import RarityGradient from '../../components/rarity-gradient';
 
 import Layout from '../../components/layout';
-import { Box, Image, Heading, useColorModeValue, Tooltip, SimpleGrid } from '@chakra-ui/react';
+import {
+  Button,
+  Box,
+  Image,
+  Heading,
+  useColorModeValue,
+  Tooltip,
+  SimpleGrid,
+} from '@chakra-ui/react';
 import { getAllStarAtlasMarkets } from '../api/data/staratlas/markets';
 import { getMarketData } from '../api/data/staratlas/markets/[marketId]';
+import { useState } from "react";
 
 const Page = ({ item = {}, marketData = {}, id }) => {
   // TODO: if item/marketData are undefined/null then we should gracefully display an error message
   // let bg = useColorModeValue('green.200', 'red.500')
+
+  const [zoomDomain, setZoomDomain] = useState({})
+  const [selectedDomain, setSelectedDomain] = useState({})
   const colors = ['blue', 'red', 'green', 'orange', 'purple', 'teal', 'yellow'];
   const lightBg = useColorModeValue('gray.200', 'gray.700');
+  const strokeColor = useColorModeValue('#7956DD', '#B399FF');
+  const selectedStrokeColor = useColorModeValue('tomato', 'tomato');
+  const btnColor = useColorModeValue('titan', 'titanLight');
   const gridImage = useColorModeValue('/grid-light.jpg', '/dark-grid.jpg');
   let data = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((index) => {
     const scaledIndex = Math.floor(index % 7);
@@ -27,100 +42,135 @@ const Page = ({ item = {}, marketData = {}, id }) => {
     };
   });
 
+
+  function handleZoom(domain) {
+    setSelectedDomain( domain );
+  }
+
+  function handleBrush(domain) {
+    setZoomDomain( domain );
+  }
+
   return (
     <Layout title={item.name}>
-      <Box position="relative">
-        <Image
-          src={item.image || '#'}
-          width="100%"
-          height={[200, 300, '400px']}
-          loading="lazy"
-          objectFit="cover"
-        />
+      <Box p={5}>
+        <SimpleGrid columns={[1, 1, 2]}>
+          <Box>
+            <Box position="relative" height="300px" roundedTop="lg" overflow="hidden">
+              <Box position="absolute" p={[5, 5, 10]} px={[5, 5, 5]} bottom={0}>
+                <Heading color="gray.300" size="lg" mb={2}>
+                  {item.symbol}
+                </Heading>
 
-        <Box position="absolute" p={[10, 10, 20]} px={[8, 8, 10]} bottom={0}>
-          <Heading color="gray.200" size="4xl">
-            {item.name}
-          </Heading>
+                <Rarity val={item.attributes.rarity} />
+              </Box>
 
-          <Heading color="gray.300" size="lg" mb={2}>
-            {item.symbol}
-          </Heading>
-
-          <Rarity val={item.attributes.rarity} />
-        </Box>
-      </Box>
-
-      {/* Victory Chart */}
-      {/* /item/asdfakjsndfkjandsf/movement */}
-      <Box>
-        <RarityGradient val={item.attributes.rarity} />
-      </Box>
-
-      <SimpleGrid>
-      <Box p={[5, 5, 10]}>
-        <Box mb={2}>{item.description}</Box>
-
-        <Box height={[300, 400, 500, 600]} width={[300, 400, 500, 600]} position="relative">
-          <Image src={gridImage} width="100%" height="100%" zIndex={1} position="relative" />
-
-          {['green', 'blue', 'red', 'yellow', 'orange', 'gray'].map((el, idx) => {
-            return <Box key={ idx + el}> <Bubble el={el} idx={idx} />
+              <Image
+                src={item.image || '#'}
+                width="100%"
+                height={['100%']}
+                loading="lazy"
+                objectFit="cover"
+              />
             </Box>
-          })}
-        </Box>
 
-        
-      </Box>
-
-      <Box p={[5, 5, 10]}>
-        <Box mb={2}>{item.description}</Box>
-
-        <Box height={[300, 400, 500, 600]} width={[300, 400, 500, 600]} position="relative">
-          <Image src={gridImage} width="100%" height="100%" zIndex={1} position="relative" />
-
-          {['green', 'blue', 'red', 'yellow', 'orange', 'gray'].map((el, idx) => {
-            return <Box key={ idx + el}> <Bubble el={el} idx={idx} />
+            <Box mb={2}>
+              <RarityGradient val={item.attributes.rarity} />
             </Box>
-          })}
-        </Box>
 
-        
-      </Box>
-      </SimpleGrid>
-      
+            <Heading size="xl" mb={2}>
+              {item.name}
+            </Heading>
 
-      <Box height="520px" width="520px" bg="white" rounded="md" display="none">
-          <VictoryChart animate={{ duration: 1200, easing: 'bounceIn' }}>
-            <VictoryScatter
-              data={data}
+            <Box mb={4} fontSize="sm">{item.description}</Box>
+
+           
+          </Box>
+
+          <Box px={5}>
+
+          <Button colorScheme="purpe" bg={btnColor} width="100%" mb={2} size="lg" color="white">
+              Place a bid
+            </Button>
+
+            <Button colorScheme="purpe" variant="outline" width="100%" size="lg" mb={2}>
+              Buy it now
+            </Button>
+
+            <VictoryChart
+            width={500}
+            height={300}
+            scale={{x: "time"}}
+            containerComponent={
+              <VictoryZoomContainer responsive={false}
+                zoomDimension="x"
+                zoomDomain={zoomDomain}
+                onZoomDomainChange={handleZoom.bind(this)}
+              />
+            }
+          >
+            <VictoryLine
               style={{
-                data: {
-                  fill: ({ datum }) => datum.fill,
-                  opacity: ({ datum }) => datum.opacity,
-                },
+                data: {stroke: strokeColor }
               }}
+              data={[
+                {x: new Date(2020, 1, 1), y: 125},
+                {x: new Date(2020, 4, 1), y: 257},
+                {x: new Date(2020, 7, 1), y: 345},
+                {x: new Date(2020, 10, 1), y: 515},
+                {x: new Date(2021, 1, 1), y: 132},
+                {x: new Date(2021, 4, 1), y: 305},
+                {x: new Date(2021, 7, 1), y: 270},
+                {x: new Date(2021, 10, 1), y: 470}
+              ]}
             />
 
+          </VictoryChart>
+
+          <VictoryChart
+            width={500}
+            height={90}
+            scale={{x: "time"}}
+            padding={{top: 0, left: 50, right: 50, bottom: 30}}
+            containerComponent={
+              <VictoryBrushContainer responsive={false}
+                brushDimension="x"
+                brushDomain={selectedDomain}
+                onBrushDomainChange={handleBrush.bind(this)}
+              />
+            }
+          >
             <VictoryAxis
-              crossAxis
-              width={520}
-              height={520}
-              domain={[-2, 2]}
-              // theme={VictoryTheme.material}
-              standalone={false}
+              tickValues={[
+                new Date(2020, 1, 1),
+                new Date(2020, 4, 1),
+                new Date(2020, 7, 1),
+                new Date(2020, 10, 1),
+                new Date(2021, 1, 1),
+                new Date(2021, 4, 1),
+                new Date(2021, 7, 1)
+              ]}
+              tickFormat={(x) => new Date(x).getFullYear()}
             />
-            <VictoryAxis
-              dependentAxis
-              crossAxis
-              width={320}
-              height={220}
-              domain={[-2, 2]}
-              // theme={VictoryTheme.material}
-              standalone={false}
+            <VictoryLine
+              style={{
+                data: {stroke: selectedStrokeColor}
+              }}
+              data={[
+                {x: new Date(2020, 1, 1), y: 125},
+                {x: new Date(2020, 4, 1), y: 257},
+                {x: new Date(2020, 7, 1), y: 345},
+                {x: new Date(2020, 10, 1), y: 515},
+                {x: new Date(2021, 1, 1), y: 132},
+                {x: new Date(2021, 4, 1), y: 305},
+                {x: new Date(2021, 7, 1), y: 270},
+                {x: new Date(2021, 10, 1), y: 470}
+              ]}
             />
           </VictoryChart>
-        </Box>
+          </Box>
+        </SimpleGrid>
+      </Box>
     </Layout>
   );
 };
@@ -147,14 +197,12 @@ function Bubble({ el, idx }) {
   const bg = useColorModeValue(el + '.400', el + '.200');
   const size = Math.random() * 100 + idx;
   return (
-    <Tooltip label={<Box height="200px" width="200px">
-      
-    </Box>}>
+    <Tooltip label={<Box height="200px" width="200px"></Box>}>
       <Box
         bg={bg}
         cursor="pointer"
-        width={ size }
-        height={ size }
+        width={size}
+        height={size}
         rounded="full"
         opacity={0.8}
         _hover={{ opacity: 0.98 }}
